beb35f30ea6c66099af748f4ce182b595fc0bced
sunsonni <sonniemnguyen@gmail.com>
2026-01-21 09:35:47 -0600

    beginning to parse

--- graph_builder1.py
+++ graph_builder1.py
@@ -1,28 +1,32 @@
-# CREATE and Design Graph Class
-# Functions: 
-#     add_node
-#     add_edge
-#     remove_node
-#     remove_edge
-#     to_adjacency_list
+# Functions from Graph: 
+#     add_node(self, city)
+#     add_edge(self, src, dst, weight)
+#     remove_node(self, city)
+#     remove_edge(self, src, dst)
+#     to_adjacency_list(self)
 
+from graph import Graph
 
-#  Testingjfdsjfdskl
+import sys
 
+try:
+    filename = sys.argv[1]
 
+    graph = Graph()
 
-
-
-def add_node():
-    print("test")
-
-def add_edge():
-        print("test")
-def remove_node():
-    print("test")
+    with open(filename, "r") as f:
+        for line in f:
+            if line.strip() in ("CITIES", "ROADS"):
+                print(line.strip())
+                continue
+            
+            
+    output = graph.to_adjacency_list()
+    print(output)
+    # print(contents)
+            
+except Exception as e:
+    print(f"Error: {e}")
     
-def remove_edge():
-    print("test")
+    
 
-def to_adjacency_list():
-    print("test") 
