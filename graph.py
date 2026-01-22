9a3f514dafc81ff8524f8314a8d55c0640e23a5b
Hayden Bueltemann <hayden.bueltemann@gmail.com>
2026-01-21 21:44:15 -0600

    reviewed graph.py

--- graph.py
+++ graph.py
@@ -1,47 +1,95 @@
 # Using Hash Table
 class Graph:
     def __init__(self):
-        # Adjacency list: {city: {neighbor_city: weight}}
-        self.adj = {}
+        self.size = 100
+        self.nodes = [None] * self.size # array for the City name
+        self.weights = [None] * self.size # array for the city details
+
+    def hash(self, key, i): # uses ASCII sum + linear probing
+        h = 0
+        
+        for c in key:
+            h += ord(c)
+        j = (h + i) % self.size
+        return j
 
     def add_node(self, city):
-        # Adds city to the graph if it doesn't exist yet
-        if city not in self.adj:
-            self.adj[city] = {}
+        i = 0 # probe counter
         
-    def add_edge(self, src, dst, weight):
-        # Adds directed road from src to dst with gvn weight
-        # Automatically adds cities if not previosly defined
+        while i < self.size:
+            j = self.hash(city, i)
+
+            # Checking for open slot
+            if self.nodes[j] is None:
+                self.nodes[j] = city # store city name
+                self.weights[j] = [] # initilaizes other array for the city details
+                return j
+            elif self.nodes[j] == city:
+                return j # already defined
+            else:
+                i += 1
+
+        raise Exception("Hash table overflow")
+
+        
+    def add_edge(self, src, dst, weight): # Adds directed edge from src to dst
+        # src = source, dst = destination
         self.add_node(src)
         self.add_node(dst)
-        self.adj[src][dst] = weight
-
-    def remove_edge(self, src, dst):
-        # Removes directed road from src to dst
-        if src in self.adj and dst in self.adj[src]:
-            del self.adj[src][dst]
-
-    def remove_node(self, city):
-        # Removes city and all of its connecting roads
-        if city in self.adj:
-            del self.adj[city]
         
-        for src in self.adj:
-            if city in self.adj[src]:
-                del self.adj[src][city]
+        # finding src index
+        i = 0
+        while i < self.size:
+            j = self.hash(src,i)
+            
+            if self.nodes[j] == src:
+                self.weights[j].append((dst,weight))
+                return
+            i += 1
+        
+        raise Exception(f"Source city {src} not found")
+
+    def remove_edge(self, src, dst): # Removes directed road from src to dst
+        i = 0
+        while i < self.size:
+            j = self.hash(src,i)
+
+            if self.nodes[j] == src:
+                tmp = [(c, w)
+                    for (c, w) in self.weights[j] # c = neighboring city, w = its weight
+                    if c != dst]
+                self.weights[j] = tmp
+                return
+            i += 1
+
+    def remove_node(self, city): # Removes city and all of its connecting roads
+        i = 0
+        while i < self.size:
+            j = self.hash(city, i)
+
+            if self.nodes[j] == city:
+                self.nodes[j] = None
+                self.weights[j] = None
+                break
+            i += 1
+        
+        # remove city from any remaining edges
+        for n in range(self.size):
+            if self.weights[n] is not None:
+                tmp = [(c, w)
+                       for (c, w) in self.weights[n]
+                       if c != city]
+                self.weights[n] = tmp
     
-    def to_adjacency_list(self):
-        # Returns string of graph as adjacnecy list
+    def to_adjacency_list(self): # Returns string representation of graph as list
         lines = []
-
-        for src in self.adj:
-            edges = []
-            for dst, weight in self.adj[src].items():
-                edges.append(f"{dst}({weight})")
-
-            if edges:
-                lines.append(f"{src}: {', '.join(edges)}")
-            else:
-                lines.append(f"{src}:")
-                
+        
+        for i in range(self.size):
+            if self.nodes[i] is not None:
+                edges = [f"{c}({w})"
+                     for (c,w) in self.weights[i]]
+                if edges:
+                    lines.append(f"{self.nodes[i]}: {', '.join(edges)}")
+                else:
+                    lines.append(f"{self.nodes[i]}:")
         return "\n".join(lines)
